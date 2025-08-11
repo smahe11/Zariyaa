0912ab6f751c64ca74a89cf940dc450a418e6dcb
smahe11 <smahe11@gmail.com>
2025-08-11 16:11:10 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
-import Zariyaa from "../components/Zariyaa";
+import Zariyaa from "../components/ui/Zariyaa";
+
+// (Only if the file name is actually Zariya.tsx with one 'a', use this instead)
+// import Zariyaa from "../components/ui/Zariya";
 
 export default function Page() {
   return <Zariyaa />;
